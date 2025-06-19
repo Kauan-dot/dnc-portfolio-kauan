5771760cbb1d3171793d9af6cf2842ba4b8cccef
Kauan-dot <kauanaffonso115@gmail.com>
2025-06-19 14:55:31 -0300

    Feat: Adding Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import styled from 'styled-components'
+
+const Header = styled.div`
+  background-color: #0f1624;
+  padding: 20px;
+  color: white;
+
+  ul {
+    list-style: none;
+    display: flex;
+    padding: 0;
+
+    li {
+      margin: 0 10px;
+
+      a {
+        color: white;
+        text-decoration: none;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+`
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Header>
+        <ul>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+          <li>
+            <a href="#">Sobre mim</a>
+          </li>
+        </ul>
+      </Header>
+    </>
+  )
 }
 
 export default App
